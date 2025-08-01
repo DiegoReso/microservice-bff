df0c5ed0932525e45a785746eb8053f3598497f5
diego reis <diegoreso@me.com>
2025-08-01 15:21:20 -0300

    project created

---
+++ src/main/java/com/reso/bffscheduler/BffSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.reso.bffscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BffSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reso/bffscheduler/BffSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reso.bffscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
